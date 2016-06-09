0c7048a15c5685af4ac2ed610e2cc7ef5f5eb4f6
Matija Pretnar <matija@pretnar.info>
2016-06-09 16:23:21 +0200

    Eratostenovo rešeto v zbirniku

---
+++ 14-zbirnik/prastevila.asm
@@ -0,0 +1,29 @@
+PUSH 2
+PUSH 3
+MOV C, 3
+
+naslednje_stevilo:
+ADD C, 2
+
+MOV D, 230
+
+naslednji_delitelj:
+MOV A, [D]
+MUL A
+JC je_prastevilo
+CMP A, C
+JA je_prastevilo
+
+MOV A, C
+DIV [D]
+MUL [D]
+CMP A, C
+JE naslednje_stevilo
+
+DEC D
+
+JMP naslednji_delitelj
+
+je_prastevilo:
+PUSH C
+JMP naslednje_stevilo
