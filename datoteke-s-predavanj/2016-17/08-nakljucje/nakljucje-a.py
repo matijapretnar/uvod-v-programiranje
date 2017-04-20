6fc912a16ef9ced65594156ba2e7338ce0b17464
Matija Pretnar <matija@pretnar.info>
2017-04-20 09:05:25 +0200

    Datoteke z Naključje B

--- datoteke-s-predavanj/2016-17/08-nakljucje/nakljucje-a.py
+++ datoteke-s-predavanj/2016-17/08-nakljucje/nakljucje-a.py
@@ -9,3 +9,16 @@ def neposteni_kovanec(verjetnost_grba):
         return 'grb'
     else:
         return 'cifra'
+
+def oceni_pi(stevilo_poskusov):
+    v_krogu = 0
+    poskusov_do_zdaj = 0
+    for i in range(stevilo_poskusov):
+        x = random.random()
+        y = random.random()
+        if x ** 2 + y ** 2 <= 1:
+            v_krogu += 1
+        poskusov_do_zdaj += 1
+#        if i % 10000 == 0:
+#            print(i, 4 * v_krogu / poskusov_do_zdaj)
+    return 4 * v_krogu / stevilo_poskusov
