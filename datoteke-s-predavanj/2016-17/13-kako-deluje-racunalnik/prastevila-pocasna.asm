2473526e360afd6246e9c3ad0e29f01f5b022e71
Matija Pretnar <matija@pretnar.info>
2017-06-12 14:09:59 +0200

    Programi za iskanje praštevil

---
+++ datoteke-s-predavanj/2016-17/13-kako-deluje-racunalnik/prastevila-pocasna.asm
@@ -0,0 +1,32 @@
+PUSH 2
+MOV C, 3
+
+preveri_ali_je_C_prastevilo:
+; v D bomo shranjevali možne delitelje
+; prvi možni je 3
+MOV D, 3
+
+  preveri_ali_D_deli_C:
+    ; če je D večji od C, je C praštevilo
+    CMP C, D
+    JBE C_je_prastevilo
+
+    ; sicer pogledamo, če D deli C
+    MOV A, C
+    DIV D
+    MUL D
+    CMP A, C
+
+    ; če D deli C, potem pojdi na naslednji C
+    JE pojdi_na_naslednji_C
+
+    ; če D ne deli C, povečaj D in ponovi vajo
+    ADD D, 2
+    JMP preveri_ali_D_deli_C
+
+  C_je_prastevilo:
+    PUSH C
+
+pojdi_na_naslednji_C:
+ADD C, 2
+JMP preveri_ali_je_C_prastevilo
