329961a8ed40104ae93b5b924d32d5c1d6418df6
Matija Pretnar <matija@pretnar.info>
2018-02-19 14:02:47 +0100

    Popravki prosojnic

--- prosojnice/pomozne-datoteke/prosojnice.js
+++ prosojnice/pomozne-datoteke/prosojnice.js
@@ -1,16 +1,39 @@
-$(document).ready(function() { 
+var jQuerySrc = document.createElement('script');
+jQuerySrc.src = 'https://code.jquery.com/jquery-1.12.4.min.js';
+jQuerySrc.type = 'text/javascript';
+jQuerySrc.onload = function() {
+    var $ = window.jQuery;
+    $(document).ready(function() { 
 
-  $(".spoiler").hide();
-  
-  $('<a class="reveal">???</a>').insertBefore('.spoiler');
+      $('span.spoiler').hide();
 
-  $("a.reveal").click(function(){
-    $(this).next().show();
-    $(this).hide();
-  });
+      $('<a class="reveal">???</a>').insertBefore('.spoiler');
 
-}); 
+      $('a.reveal').click(function(){
+        $(this).next().fadeIn(100);
+        $(this).hide();
+      });
 
-var slideshow = remark.create({
-  highlightLines: true
-});
+      $('.terminal').addClass('hljs-hybrid');
+    }); 
+};
+document.head.appendChild(jQuerySrc);
+
+var remarkSrc = document.createElement('script');
+remarkSrc.src = 'https://remarkjs.com/downloads/remark-latest.min.js';
+remarkSrc.type = 'text/javascript';
+remarkSrc.onload = function () {
+  var slideshow = remark.create({
+    highlightLines: true,
+    highlightLanguage: 'python',
+    highlightStyle: 'github',
+    countIncrementalSlides: false,
+  });          
+};
+document.head.appendChild(remarkSrc);
+
+var mathJaxSrc = document.createElement('script');
+mathJaxSrc.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
+mathJaxSrc.type = 'text/javascript';
+document.head.appendChild(mathJaxSrc);
+
