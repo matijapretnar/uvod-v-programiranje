03dacbcae1639f10c17687f82a2e1269c0aa67a6
Matija Pretnar <matija@pretnar.info>
2020-05-12 09:49:54 +0200

    Dostop do modela izkljucno preko metod na Proracun

--- datoteke-s-predavanj/2019-20/proracun/tekstovni_vmesnik.py
+++ datoteke-s-predavanj/2019-20/proracun/tekstovni_vmesnik.py
@@ -1,21 +1,20 @@
 from datetime import date
-from model import Proracun, Racun, Kuverta, Preliv
+from model import Proracun
 
 proracun = Proracun()
 
 gotovina = proracun.dodaj_racun('gotovina')
 tekoci_racun = proracun.dodaj_racun('tekoči račun')
-
 vreca = proracun.dodaj_kuverto('💰')
 
-Preliv(230, date(2020, 4, 1), 'štipendija', tekoci_racun, vreca)
-Preliv(-30, date(2020, 4, 3), 'prevoz', tekoci_racun, vreca)
-Preliv(-40, date(2020, 4, 5), 'hlače', gotovina, vreca)
-Preliv(150, date(2020, 4, 20), 'krizni dodatek', tekoci_racun, vreca)
-Preliv(-150, date(2020, 4, 20), 'hlače', tekoci_racun, vreca)
-Preliv(-100, date(2020, 4, 30), 'najemnina', tekoci_racun, vreca)
-Preliv(-10, date(2020, 4, 30), 'telefon', tekoci_racun, vreca)
-Preliv(-40, date(2020, 5, 4), 'hrana', gotovina, vreca)
+proracun.dodaj_preliv(230, date(2020, 4, 1), 'štipendija', tekoci_racun, vreca)
+proracun.dodaj_preliv(-30, date(2020, 4, 3), 'prevoz', tekoci_racun, vreca)
+proracun.dodaj_preliv(-40, date(2020, 4, 5), 'hlače', gotovina, vreca)
+proracun.dodaj_preliv(150, date(2020, 4, 20), 'krizni dodatek', tekoci_racun, vreca)
+proracun.dodaj_preliv(-150, date(2020, 4, 20), 'hlače', tekoci_racun, vreca)
+proracun.dodaj_preliv(-100, date(2020, 4, 30), 'najemnina', tekoci_racun, vreca)
+proracun.dodaj_preliv(-10, date(2020, 4, 30), 'telefon', tekoci_racun, vreca)
+proracun.dodaj_preliv(-40, date(2020, 5, 4), 'hrana', gotovina, vreca)
 
 def vnesi_stevilo(pozdrav):
     while True:
--- datoteke-s-predavanj/2019-20/proracun/test.py
+++ datoteke-s-predavanj/2019-20/proracun/test.py
@@ -1,20 +1,19 @@
 from datetime import date
-from model import Proracun, Racun, Kuverta, Preliv
+from model import Proracun
 
-moj_proracun = Proracun()
+proracun = Proracun()
 
-gotovina = moj_proracun.dodaj_racun('gotovina')
-tekoci_racun = moj_proracun.dodaj_racun('tekoči račun')
+gotovina = proracun.dodaj_racun('gotovina')
+tekoci_racun = proracun.dodaj_racun('tekoči račun')
+vreca = proracun.dodaj_kuverto('💰')
 
-vreca = moj_proracun.dodaj_kuverto('💰')
+proracun.dodaj_preliv(230, date(2020, 4, 1), 'štipendija', tekoci_racun, vreca)
+proracun.dodaj_preliv(-30, date(2020, 4, 3), 'prevoz', tekoci_racun, vreca)
+proracun.dodaj_preliv(-40, date(2020, 4, 5), 'hlače', gotovina, vreca)
+proracun.dodaj_preliv(150, date(2020, 4, 20), 'krizni dodatek', tekoci_racun, vreca)
+proracun.dodaj_preliv(-150, date(2020, 4, 20), 'hlače', tekoci_racun, vreca)
+proracun.dodaj_preliv(-100, date(2020, 4, 30), 'najemnina', tekoci_racun, vreca)
+proracun.dodaj_preliv(-10, date(2020, 4, 30), 'telefon', tekoci_racun, vreca)
+proracun.dodaj_preliv(-40, date(2020, 5, 4), 'hrana', gotovina, vreca)
 
-Preliv(230, date(2020, 4, 1), 'štipendija', tekoci_racun, vreca)
-Preliv(-30, date(2020, 4, 3), 'prevoz', tekoci_racun, vreca)
-Preliv(-40, date(2020, 4, 5), 'hlače', gotovina, vreca)
-Preliv(150, date(2020, 4, 20), 'krizni dodatek', tekoci_racun, vreca)
-Preliv(-150, date(2020, 4, 20), 'hlače', tekoci_racun, vreca)
-Preliv(-100, date(2020, 4, 30), 'najemnina', tekoci_racun, vreca)
-Preliv(-10, date(2020, 4, 30), 'telefon', tekoci_racun, vreca)
-Preliv(-40, date(2020, 5, 4), 'hrana', gotovina, vreca)
-
-print(moj_proracun)
+print(proracun)
