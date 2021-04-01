46d3c6310bed5cd0b125f9157bbe7f84559a4c17
Matija Pretnar <matija@pretnar.info>
2021-04-01 19:49:19 +0200

    Datoteke s predavanj

--- 07-razredi/iteratorji.py
+++ 07-razredi/iteratorji.py
@@ -10,12 +10,34 @@ class SprehajalecPoRazponu:
         else:
             raise StopIteration
 
+class SprehajalecPoNizu:
+    def __init__(self, niz):
+        self.kje_sem = 0
+        self.kje_bom_koncal = len(niz)
+        self.niz = niz
+    
+    def __next__(self):
+        if self.kje_sem < self.kje_bom_koncal:
+            self.kje_sem += 1
+            return self.niz[self.kje_sem - 1]
+        else:
+            raise StopIteration
+    
+    def __iter__(self):
+        return self
+
 class Razpon:
     def __init__(self, od, do):
         self.od = od
         self.do = do
-        x = 10
 
     def __iter__(self):
         sprehajalec = SprehajalecPoRazponu(self.od, self.do)
         return sprehajalec
+
+
+def fibonacci(m):
+    a, b = 0, 1
+    while a <= m:
+        yield a
+        a, b = b, a + b
--- 07-razredi/zgoscevalne_tabele.py
+++ 07-razredi/zgoscevalne_tabele.py
@@ -25,9 +25,8 @@ class Mnozica:
 
     def _preuredi(self):
         nova_mnozica = Mnozica(n=2 * len(self.skatle))
-        for skatla in self.skatle:
-            for n in skatla:
-                nova_mnozica._dodaj_brez_preurejanja(n)
+        for n in self:
+            nova_mnozica._dodaj_brez_preurejanja(n)
         self.skatle = nova_mnozica.skatle
 
     def __contains__(self, n):
@@ -36,26 +35,23 @@ class Mnozica:
     def __eq__(self, other):
         if self.st_elementov != other.st_elementov:
             return False
-        for skatla in self.skatle:
-            for x in skatla:
-                if x not in other:
-                    return False
+        for x in self:
+            if x not in other:
+                return False
         return True
 
     def __repr__(self):
-        seznam_elementov = []
-        for skatla in self.skatle:
-            for x in skatla:
-                seznam_elementov.append(x)
-        return f"Mnozica({seznam_elementov})"
+        return f"Mnozica({list(self)})"
 
     def __str__(self):
-        seznam_elementov = []
+        elementi_loceni_z_vejicami = ", ".join([str(x) for x in self])
+        return f"{{{elementi_loceni_z_vejicami}}}"
+    
+    def __iter__(self):
         for skatla in self.skatle:
             for x in skatla:
-                seznam_elementov.append(str(x))
-        elementi_loceni_z_vejicami = ", ".join(seznam_elementov)
-        return f"{{{elementi_loceni_z_vejicami}}}"
+                yield x
+
 
 
 
@@ -63,7 +59,7 @@ class Mnozica:
 import random
 m = Mnozica()
 s = []
-for x in range(1000):
+for x in range(20):
     y = random.randint(-1000 * x, 1000 * x)
     m.dodaj(y)
     # m.
