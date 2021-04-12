7903df5307e28cd68aea084c2495271e3abf89c7
Matija Pretnar <matija@pretnar.info>
2021-04-12 16:10:17 +0200

    Datoteke s predavanj

---
+++ projekti/tudu/model.py
@@ -0,0 +1,34 @@
+from datetime import date
+
+class Model:
+    def __init__(self):
+        self.spisek = Spisek("Edini spisek")
+
+class Spisek:
+    def __init__(self, ime):
+        self.ime = ime
+        self.opravila = []
+    
+    def dodaj_opravilo(self, opravilo):
+        self.opravila.append(opravilo)
+    
+    def stevilo_zamujenih(self):
+        stevilo = 0
+        for opravilo in self.opravila:
+            if opravilo.zamuja():
+                stevilo += 1
+        return stevilo
+
+class Opravilo:
+    def __init__(self, ime, opis, rok):
+        self.ime = ime
+        self.opis = opis
+        self.rok = rok
+        self.opravljeno = False
+
+    def opravi(self):
+        self.opravljeno = True
+    
+    def zamuja(self):
+        rok_pretekel = self.rok and self.rok < date.today()
+        return not self.opravljeno and rok_pretekel
---
+++ projekti/tudu/tekstovni_vmesnik.py
@@ -0,0 +1,7 @@
+model = Model()
+
+def tekstovni_vmesnik():
+    while True:
+        izpisi_opravila()
+        preberi_ukaz()
+
