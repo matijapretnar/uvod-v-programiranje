1e15e750a5b605ee831ccf49b23ea95793d578e7
Matija Pretnar <matija@pretnar.info>
2021-04-19 15:03:45 +0200

    Začel pisati strukturo tekstovnega vmesnika

--- projekti/opravila/tekstovni_vmesnik.py
+++ projekti/opravila/tekstovni_vmesnik.py
@@ -1,7 +1,38 @@
-model = Model()
+DODAJ_SPISEK = 1
+POBRISI_SPISEK = 2
+ZAMENJAJ_SPISEK = 3
+DODAJ_OPRAVILO = 4
+POBRISI_OPRAVILO = 5
+OPRAVI_OPRAVILO = 6
+IZHOD = 7
 
 def tekstovni_vmesnik():
+    prikazi_pozdravno_sporocilo()
     while True:
-        izpisi_opravila()
-        preberi_ukaz()
+        prikazi_aktualna_opravila()
+        ukaz = izberi_moznost([
+            (DODAJ_SPISEK, "dodaj nov spisek"),
+            (POBRISI_SPISEK, "pobriši spisek"),
+            (ZAMENJAJ_SPISEK, "prikaži drug spisek"),
+            (DODAJ_OPRAVILO, "dodaj novo opravilo"),
+            (POBRISI_OPRAVILO, "pobriši opravilo"),
+            (OPRAVI_OPRAVILO, "opravi opravilo"),
+            (IZHOD, "zapri program"),
+        ])
+        if ukaz == DODAJ_SPISEK:
+            dodaj_spisek()
+        elif ukaz == POBRISI_SPISEK:
+            pobrisi_spisek()
+        elif ukaz == ZAMENJAJ_SPISEK:
+            zamenjaj_spisek()
+        elif ukaz == DODAJ_OPRAVILO:
+            dodaj_opravilo()
+        elif ukaz == POBRISI_OPRAVILO:
+            pobrisi_opravilo()
+        elif ukaz == OPRAVI_OPRAVILO:
+            opravi_opravilo()
+        elif ukaz == IZHOD:
+            print('Nasvidenje!')
+            break
 
+tekstovni_vmesnik()
