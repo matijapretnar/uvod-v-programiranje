57c1f8e98327f35624977b321b5b9d144a19c8c8
Matija Pretnar <matija@pretnar.info>
2021-04-20 10:00:02 +0200

    Dodal pomožne funkcije

--- projekti/kuverte/tekstovni_vmesnik.py
+++ projekti/kuverte/tekstovni_vmesnik.py
@@ -16,6 +16,83 @@ zadnji_nakup = Preliv(15, 'špageti in pivo', date(2021, 4, 12))
 gotovina.dodaj_preliv(zadnji_nakup)
 hrana.dodaj_preliv(zadnji_nakup)
 
+###########################################################
+# Pomožne funkcije za prikaz
+###########################################################
+
+def krepko(niz):
+    return f'\033[1m{niz}\033[0m'
+
+def dobro(niz):
+    return f'\033[1;94m{niz}\033[0m'
+
+def slabo(niz):
+    return f'\033[1;91m{niz}\033[0m'
+
+def prikaz_zneska(ime, stanje):
+    if stanje > 0:
+        return f'{ime}: {dobro(stanje)} €'
+    elif stanje < 0:
+        return f'{ime}: {slabo(stanje)} €'
+    else:
+        return f'{ime}: 0 €'
+
+def prikaz_racuna(racun):
+    return prikaz_zneska(racun.ime, racun.stanje())
+
+def prikaz_kuverte(kuverta):
+    if kuverta is None:
+        return prikaz_zneska('nerazporejeno', proracun.nerazporejena_sredstva())
+    else:
+        return prikaz_zneska(kuverta.ime, kuverta.stanje())
+
+###########################################################
+# Pomožne funkcije za vnos
+###########################################################
+
+def vnesi_stevilo(pozdrav):
+    """S standardnega vhoda prebere naravno število."""
+    while True:
+        try:
+            stevilo = input(pozdrav)
+            return int(stevilo)
+        except ValueError:
+            print(slabo('Prosim, da vnesete število!'))
+
+
+def izberi(seznam):
+    """
+    Uporabniku omogoči interaktivno izbiro elementa iz seznama.
+    
+    Funkcija sprejme seznam parov (oznaka, element), prikaže seznam
+    oznak ter vrne element, ki ustreza vpisani oznaki.
+    >>> izberi([('deset', 10), ('trideset', 30)])
+    1) deset
+    2) trideset
+    > 2
+    30
+    """
+    for indeks, (oznaka, _) in enumerate(seznam, 1):
+        print(f'{indeks}) {oznaka}')
+    while True:
+        izbira = vnesi_stevilo('> ')
+        if 1 <= izbira <= len(seznam):
+            _, element = seznam[izbira - 1]
+            return element
+        else:
+            print(slabo(f'Izberi število med 1 in {len(seznam)}'))
+
+
+def izberi_kuverto(kuverte):
+    return izberi([(prikaz_kuverte(kuverta), kuverta) for kuverta in kuverte])
+
+def izberi_racun(racuni):
+    return izberi([(prikaz_racuna(racun), racun) for racun in racuni])
+
+###########################################################
+# Tekstovni vmesnik
+###########################################################
+
 def tekstovni_vmesnik():
     uvodni_pozdrav()
     while True:
