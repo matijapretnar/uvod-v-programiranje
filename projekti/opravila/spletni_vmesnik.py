58d5b9e3ab8646cdc94454348b08e573bb9dfce4
Matija Pretnar <matija@pretnar.info>
2021-05-11 09:12:21 +0200

    Nadgraditev opravil na spletno storitev

--- projekti/opravila/spletni_vmesnik.py
+++ projekti/opravila/spletni_vmesnik.py
@@ -2,22 +2,51 @@ import bottle
 from datetime import date
 from model import Stanje, Opravilo, Spisek
 
-IME_DATOTEKE = "stanje.json"
-try:
-    stanje = Stanje.preberi_iz_datoteke(IME_DATOTEKE)
-except FileNotFoundError:
-    stanje = Stanje()
+
+def nalozi_uporabnikovo_stanje():
+    uporabnisko_ime = bottle.request.get_cookie("uporabnisko_ime")
+    if uporabnisko_ime:
+        try:
+            stanje = Stanje.preberi_iz_datoteke(uporabnisko_ime)
+        except FileNotFoundError:
+            stanje = Stanje()
+        return stanje
+    else:
+        bottle.redirect('/prijava/')
+
+def shrani_uporabnikovo_stanje(stanje):
+    uporabnisko_ime = bottle.request.get_cookie("uporabnisko_ime")
+    stanje.shrani_v_datoteko(uporabnisko_ime)
 
 
 @bottle.get("/")
 def osnovna_stran():
+    stanje = nalozi_uporabnikovo_stanje()
     return bottle.template(
         "osnovna_stran.html",
         neopravljena=stanje.stevilo_zamujenih(),
         opravila=stanje.aktualni_spisek.opravila if stanje.aktualni_spisek else [],
         spiski=stanje.spiski,
-        aktualni_spisek=stanje.aktualni_spisek
+        aktualni_spisek=stanje.aktualni_spisek,
+        uporabnisko_ime = bottle.request.get_cookie("uporabnisko_ime")
     )
+
+@bottle.get("/prijava/")
+def prijava_get():
+    return bottle.template("prijava.html", napake={}, polja={}, uporabnisko_ime=None)
+
+
+@bottle.post("/prijava/")
+def prijava_post():
+    uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
+    bottle.response.set_cookie("uporabnisko_ime", uporabnisko_ime, path="/")
+    bottle.redirect("/")
+
+@bottle.post("/odjava/")
+def odjava_post():
+    bottle.response.delete_cookie("uporabnisko_ime", path="/")
+    print("piškotek uspešno pobrisan")
+    bottle.redirect("/")
 
 
 @bottle.post("/dodaj/")
@@ -29,8 +58,9 @@ def dodaj_opravilo():
     else:
         datum = None
     opravilo = Opravilo(ime, opis, datum)
+    stanje = nalozi_uporabnikovo_stanje()
     stanje.dodaj_opravilo(opravilo)
-    stanje.shrani_v_datoteko(IME_DATOTEKE)
+    shrani_uporabnikovo_stanje(stanje)
     bottle.redirect("/")
 
 @bottle.get("/dodaj-spisek/")
@@ -41,22 +71,24 @@ def dodaj_spisek_get():
 def dodaj_spisek_post():
     ime = bottle.request.forms.getunicode("ime")
     polja = {"ime": ime}
+    stanje = nalozi_uporabnikovo_stanje()
     napake = stanje.preveri_podatke_novega_spiska(ime)
     if napake:
         return bottle.template("dodaj_spisek.html", napake=napake, polja=polja)
     else:
         spisek = Spisek(ime)
         stanje.dodaj_spisek(spisek)
-        stanje.shrani_v_datoteko(IME_DATOTEKE)
+        shrani_uporabnikovo_stanje(stanje)
         bottle.redirect("/")
 
 
 @bottle.post("/zamenjaj-opravljeno/")
 def zamenjaj_opravljeno():
     indeks = bottle.request.forms.getunicode("indeks")
+    stanje = nalozi_uporabnikovo_stanje()
     opravilo = stanje.aktualni_spisek.opravila[int(indeks)]
     opravilo.zamenjaj_opravljeno()
-    stanje.shrani_v_datoteko(IME_DATOTEKE)
+    shrani_uporabnikovo_stanje(stanje)
     bottle.redirect("/")
 
 
@@ -64,9 +96,10 @@ def zamenjaj_opravljeno():
 def zamenjaj_aktualni_spisek():
     print(dict(bottle.request.forms))
     indeks = bottle.request.forms.getunicode("indeks")
+    stanje = nalozi_uporabnikovo_stanje()
     spisek = stanje.spiski[int(indeks)]
     stanje.aktualni_spisek = spisek
-    stanje.shrani_v_datoteko(IME_DATOTEKE)
+    shrani_uporabnikovo_stanje(stanje)
     bottle.redirect("/")
 
 
